6f21dda76812fdb9b9b645cfc66b164b93d96f07
parshencev <parshencev.vlad@gmail.com>
2017-07-07 11:57:52 +0300

    ws событие входа

--- src/events/actions/LOGIN.js
+++ src/events/actions/LOGIN.js
@@ -1,11 +1,13 @@
 module.exports = function(data, callback){
 	this.mysql.query({
-		sql: 'INSERT INTO user_sessions (user_id, session_id) SELECT u.user_id, s.session_id FROM users u LEFT JOIN sessions s ON s.session_hash = ? WHERE u.user_email = ? AND u.user_password = ? ',
+		sql: 'INSERT INTO `user_sessions` (user_id, session_id) SELECT u.user_id, s.session_id FROM `users` AS `u` LEFT JOIN `sessions` AS `s` ON s.session_hash = ? WHERE u.user_email = ? AND u.user_password = ?',
 		timeout: 1000,
 		values: [
 			data.hash,
-			data.email.toLowerCase(),
+			data.email,
 			data.password
-		]
-	}, callback);
+		] 
+	}, function(err, data){
+		err ? callback() : data.affectedRows > 0 ? callback(true) : callback();
+	})
 }
