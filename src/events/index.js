ffa2a4690bbd2c7fa278ea860ae2ad1ab9d18eb9
parshencev <parshencev.vlad@gmail.com>
2017-07-07 12:00:03 +0300

    костыль для работоспособности события

--- src/events/index.js
+++ src/events/index.js
@@ -1,4 +1,5 @@
 var Types = require('../constants/eventTypes.js');
 Object.keys(Types).forEach(function(key){
-	module.exports[key] = require('./actions/' + key + '.js');
+	if (key != Types.SET_HASH)
+		module.exports[key] = require('./actions/' + key + '.js');
 });
