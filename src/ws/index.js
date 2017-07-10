ffe409d4239ec95364858fbbbd387c346ef444c2
parshencev <parshencev.vlad@gmail.com>
2017-07-10 12:05:17 +0300

    загрузка ws

--- src/ws/index.js
+++ src/ws/index.js
@@ -2,10 +2,15 @@ var Types = require("../constants/eventTypes.js");
 
 module.exports = function(io, reducer, actions){
 	io.on('connection', function(socket){
+		reducer(actions[Types.HASH](), function(data){
+			if(data != null){
+				socket.emit(Types.HASH, data);
+			}
+		});
 		Object.keys(Types).forEach(function(key){
 			socket.on(key, function(data){
 				reducer(actions[key](data), function(err, responce){
-					socket.emit(key == Types.GET_HASH ? Types.SET_HASH : key, err || responce);
+					socket.emit(key, err || responce);
 				});
 			});
 		});
