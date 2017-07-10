cafaeed478e6db37ab6bbfee44056b29e48bd37c
parshencev <parshencev.vlad@gmail.com>
2017-07-10 12:00:20 +0300

    изменение собития login

--- src/events/actions/LOGIN.js
+++ src/events/actions/LOGIN.js
@@ -1,13 +1,23 @@
+var SetStatus = require('./SET_STATUS.js');
 module.exports = function(data, callback){
+	var _this = this;
+	_this.data = data;
 	this.mysql.query({
-		sql: 'INSERT INTO `user_sessions` (user_id, session_id) SELECT u.user_id, s.session_id FROM `users` AS `u` LEFT JOIN `sessions` AS `s` ON s.session_hash = ? WHERE u.user_email = ? AND u.user_password = ?',
+		sql: 'INSERT INTO `user_sessions` (`user_id`, `session_id`) SELECT u.user_id, s.session_id FROM `users` AS `u` JOIN sessions AS `s` ON s.session_hash = ? WHERE `user_email` = ? AND `user_password` = ?',
 		timeout: 1000,
 		values: [
 			data.hash,
 			data.email,
 			data.password
-		] 
+		]
 	}, function(err, data){
-		err ? callback() : data.affectedRows > 0 ? callback(true) : callback();
-	})
+		if(err)
+			callback(); 
+		else if(data.affectedRows > 0){
+			callback(true);
+			SetStatus.call({mysql: _this.mysql}, {status_id: 1, hash: _this.data.hash}, function(){});
+		}
+		else
+			callback();
+	});
 }
