6eefc5054bf8b29c52c10b6b79ce1a7af84b2aaf
parshencev <parshencev.vlad@gmail.com>
2017-07-12 16:07:08 +0300

    подготовка к обработке запросов с api.ai

--- src/rest/index.js
+++ src/rest/index.js
@@ -1,10 +1,5 @@
-var Types = require('../constants/eventTypes.js');
 module.exports = function(rest, reducer, actions){
-	Object.keys(Types).map(function(key){
-		rest.get(key, function(req, res){
-			reducer(actions[key](req.params), function(data){
-				res.send(data);
-			});
-		});
+	rest.post('/', function(request, responce){
+		console.log(request.body);
 	});
 }
