edab4bfc6068c5251cf12284d8052e7a9afd76fd
parshencev <parshencev.vlad@gmail.com>
2017-07-18 11:34:00 +0300

    описание событий в редукторе

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,6 @@
 var Events = require("./events/index.js");
 module.exports = function(mysql){
 	return function(action, callback){
-		return Events[action.type] ? Events[action.type].call({mysql}, action.data, callback || function(){}) : false;
+		return Events[action.type] ? Events[action.type].call({mysql}, action.data, callback) : false;
 	}
 }
