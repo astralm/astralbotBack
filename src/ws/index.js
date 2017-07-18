59cb0da4814b86872182047257cdf8ffc898c47f
parshencev <parshencev.vlad@gmail.com>
2017-07-18 13:57:17 +0300

    описание реакции сервера на события клиента

--- src/ws/index.js
+++ src/ws/index.js
@@ -2,9 +2,18 @@ var Types = require("../constants/eventTypes.js");
 module.exports = function(io, reducer, actions){
 	io.on('connection', function(socket){
 		socket.on(Types.LOGIN, function(data){
+			socket.email = data.email;
 			reducer(actions.LOGIN(data), function(response){
 				socket.emit(Types.LOGIN, response);
 			})
-		})
+		});
+		socket.on('disconnect', function(){
+			reducer(actions.LOGOUT({email: socket.email}));
+		});
+		socket.on(Types.LOGOUT, function(){
+			reducer(actions.LOGOUT({email: socket.email}), function(response){
+				socket.emit(Types.LOGOUT, response);
+			})
+		});
 	});
 }
