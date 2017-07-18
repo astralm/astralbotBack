f329c80b6e78b020c03dd1fed6df9064aa7fe734
parshencev <parshencev.vlad@gmail.com>
2017-07-18 11:34:47 +0300

    обработчики ws событий

--- src/ws/index.js
+++ src/ws/index.js
@@ -1,25 +1,10 @@
 var Types = require("../constants/eventTypes.js");
-
 module.exports = function(io, reducer, actions){
 	io.on('connection', function(socket){
-		reducer(actions[Types.HASH](), function(data){
-			if(data != null){
-				socket.emit(Types.HASH, data);
-				socket.hash = data;
-			}
-		});
-		socket.on('disconnect', function(){
-			reducer(actions[Types.SET_STATUS]({
-				status_id: 2,
-				hash: socket.hash
-			}), function(){});
-		});
-		Object.keys(Types).forEach(function(key){
-			socket.on(key, function(data){
-				reducer(actions[key](data), function(err, responce){
-					socket.emit(key, err || responce);
-				});
-			});
-		});
+		socket.on(Types.LOGIN, function(data){
+			reducer(actions.LOGIN(data), function(response){
+				socket.emit(Types.LOGIN, response);
+			})
+		})
 	});
 }
