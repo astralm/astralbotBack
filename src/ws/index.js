4a5fffc2b7d77df51d37222f0503253769220ab8
parshencev <parshencev.vlad@gmail.com>
2017-07-20 12:36:02 +0300

    обновление пользователей на всех клиентах

--- src/ws/index.js
+++ src/ws/index.js
@@ -1,19 +1,29 @@
 var Types = require("../constants/eventTypes.js");
+function broadcastGetUsers(){
+	var _this = this;
+	_this.reducer(_this.actions.GET_USERS(), function(response){
+		_this.io.sockets.emit(Types.GET_USERS, response);
+	});
+}
 module.exports = function(io, reducer, actions){
 	io.on('connection', function(socket){
+		broadcastGetUsers = broadcastGetUsers.bind({reducer, actions, io});
 		socket.on(Types.LOGIN, function(data){
 			socket.email = data.email;
 			reducer(actions.LOGIN(data), function(response){
 				socket.emit(Types.LOGIN, response);
 			})
+			broadcastGetUsers();
 		});
 		socket.on('disconnect', function(){
 			reducer(actions.LOGOUT({email: socket.email}));
+			broadcastGetUsers();
 		});
 		socket.on(Types.LOGOUT, function(){
 			reducer(actions.LOGOUT({email: socket.email}), function(response){
 				socket.emit(Types.LOGOUT, response);
-			})
+			});
+			broadcastGetUsers();
 		});
 		socket.on(Types.GET_USERS, function(){
 			reducer(actions.GET_USERS(), function(response){
