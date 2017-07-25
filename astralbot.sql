0c4a696a73ffed78b4456a447a8df675a45a1325
parshencev <parshencev.vlad@gmail.com>
2017-07-25 17:23:37 +0300

    исправления в базе

--- astralbot.sql
+++ astralbot.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: localhost
--- Время создания: Июл 24 2017 г., 14:42
+-- Время создания: Июл 25 2017 г., 16:22
 -- Версия сервера: 10.1.21-MariaDB
 -- Версия PHP: 5.6.30
 
@@ -35,15 +35,6 @@ CREATE TABLE `answers` (
   `session_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Дамп данных таблицы `answers`
---
-
-INSERT INTO `answers` (`answer_id`, `answer_message`, `answer_date`, `session_id`) VALUES
-(1, 'здравствуйте, чем я могу вам помочь?', '2017-07-24 08:52:27', 1),
-(2, 'здравствуйте, чем я могу вам помочь?', '2017-07-24 08:53:56', 2),
-(3, 'досвиданье', '2017-07-24 08:56:07', 1);
-
 -- --------------------------------------------------------
 
 --
@@ -57,15 +48,6 @@ CREATE TABLE `questions` (
   `session_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Дамп данных таблицы `questions`
---
-
-INSERT INTO `questions` (`question_id`, `question_message`, `question_date`, `session_id`) VALUES
-(4, 'здавствуйте', '2017-07-24 08:51:41', 1),
-(5, 'здравствуйте', '2017-07-24 08:51:59', 2),
-(6, 'ничем, спасибо', '2017-07-24 08:55:47', 1);
-
 -- --------------------------------------------------------
 
 --
@@ -76,16 +58,9 @@ CREATE TABLE `sessions` (
   `session_id` int(11) NOT NULL,
   `session_hash` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `session_status` tinyint(1) NOT NULL,
-  `user_id` int(11) NOT NULL
+  `user_id` int(11) NOT NULL,
+  `session_error` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Дамп данных таблицы `sessions`
---
-
-INSERT INTO `sessions` (`session_id`, `session_hash`, `session_status`, `user_id`) VALUES
-(1, 'jnewinwefnwef', 1, 1),
-(2, 'gewgewgwegwegweg', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -100,6 +75,7 @@ CREATE TABLE `sessions_info_view` (
 ,`session_hash` varchar(32)
 ,`session_status` tinyint(1)
 ,`user_id` int(11)
+,`session_error` tinyint(1)
 ,`user_name` varchar(32)
 );
 
@@ -117,20 +93,6 @@ CREATE TABLE `users` (
   `user_name` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Дамп данных таблицы `users`
---
-
-INSERT INTO `users` (`user_id`, `user_email`, `user_password`, `user_status`, `user_name`) VALUES
-(1, 'q', 'q', 1, 'q'),
-(2, 'w', 'w', 0, 'w'),
-(7, 'parshencev.vlad@gmail.com', '123', 0, 'Влад'),
-(8, 'q', 'q', 1, 'q'),
-(9, 'parshencev.vlad@gmail.com', '123', 0, 'Влад'),
-(10, 'e@e.e', '123123', 0, 'qwqwe'),
-(11, 'w@w.w', 'qweqe', 0, 'wqeqwe'),
-(12, 'f@d.e', 'qwd', 0, 'qdw');
-
 -- --------------------------------------------------------
 
 --
@@ -138,7 +100,7 @@ INSERT INTO `users` (`user_id`, `user_email`, `user_password`, `user_status`, `u
 --
 DROP TABLE IF EXISTS `sessions_info_view`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `sessions_info_view`  AS  select (select `questions`.`question_message` from `questions` where (`questions`.`session_id` = `s`.`session_id`) order by `questions`.`question_date` desc limit 1) AS `question`,(select `answers`.`answer_message` from `answers` where (`answers`.`session_id` = `s`.`session_id`) order by `answers`.`answer_date` desc limit 1) AS `answer`,`s`.`session_id` AS `session_id`,`s`.`session_hash` AS `session_hash`,`s`.`session_status` AS `session_status`,`s`.`user_id` AS `user_id`,`u`.`user_name` AS `user_name` from (`sessions` `s` left join `users` `u` on((`u`.`user_id` = `s`.`user_id`))) ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `sessions_info_view`  AS  select (select `questions`.`question_message` from `questions` where (`questions`.`session_id` = `s`.`session_id`) order by `questions`.`question_id` desc limit 1) AS `question`,(select `answers`.`answer_message` from `answers` where (`answers`.`session_id` = `s`.`session_id`) order by `answers`.`answer_id` desc limit 1) AS `answer`,`s`.`session_id` AS `session_id`,`s`.`session_hash` AS `session_hash`,`s`.`session_status` AS `session_status`,`s`.`user_id` AS `user_id`,`s`.`session_error` AS `session_error`,`u`.`user_name` AS `user_name` from (`sessions` `s` left join `users` `u` on((`u`.`user_id` = `s`.`user_id`))) ;
 
 --
 -- Индексы сохранённых таблиц
@@ -189,12 +151,12 @@ ALTER TABLE `questions`
 -- AUTO_INCREMENT для таблицы `sessions`
 --
 ALTER TABLE `sessions`
-  MODIFY `session_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `session_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
 -- Ограничения внешнего ключа сохраненных таблиц
 --
