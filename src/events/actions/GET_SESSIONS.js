ceea301a356ebcc28bfa7f474df443c25e274556
parshencev <parshencev.vlad@gmail.com>
2017-07-25 16:52:13 +0300

    Исправление запроса на получение полного списка сессий

--- src/events/actions/GET_SESSIONS.js
+++ src/events/actions/GET_SESSIONS.js
@@ -1,7 +1,10 @@
 module.exports = function(data, callback){
 	this.mysql.query({
-		sql: 'SELECT * FROM `sessions_info_view`',
-		timeout: 1000
+		sql: 'SELECT * FROM `sessions_info_view` LIMIT 50 OFFSET ?',
+		timeout: 1000,
+		values: [
+			data
+		]
 	}, function(err, data){
 		err ? 
 			callback() :
