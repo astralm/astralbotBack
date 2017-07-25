3a15c25e8fd144e5fc3b4e30f631d81f15128e22
parshencev <parshencev.vlad@gmail.com>
2017-07-25 16:53:27 +0300

    исправление в событии получения информации о пользователе

--- src/events/actions/UPDATE_USER.js
+++ src/events/actions/UPDATE_USER.js
@@ -1,5 +1,4 @@
 module.exports = function(data, callback){
-	console.log(data);
 	this.mysql.query({
 		sql: "SELECT * FROM `users` WHERE `user_email` = ?",
 		timeout: 1000,
@@ -7,7 +6,6 @@ module.exports = function(data, callback){
 			data
 		]
 	}, function(err, responce){
-		console.log(err, responce);
 		err ?
 			callback() :
 			callback(responce || null);
