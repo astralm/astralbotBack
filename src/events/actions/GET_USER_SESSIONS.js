0cc0d08ffc19f740c1c963c45f59fcb9b7ac2020
parshencev <parshencev.vlad@gmail.com>
2017-07-26 18:43:59 +0300

    конвертация текста в число

--- src/events/actions/GET_USER_SESSIONS.js
+++ src/events/actions/GET_USER_SESSIONS.js
@@ -3,8 +3,8 @@ module.exports = function(data, callback){
 		sql: "SELECT * FROM `sessions_info_view` WHERE `user_id` = ? LIMIT 50 OFFSET ?",
 		timeout: 1000,
 		values: [
-			data.user_id,
-			data.offset
+			+data.user_id,
+			+data.offset
 		]
 	}, function(err, data){
 		err ? 
