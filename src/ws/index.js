3e67b7834fc715c3075cbdfd56c8a58b43ddc931
parshencev <parshencev.vlad@gmail.com>
2017-07-26 18:46:09 +0300

    обработка событий отвязки и привязки сессий к пользователю, и обновление информации у всех клиентов

--- src/ws/index.js
+++ src/ws/index.js
@@ -5,9 +5,44 @@ function broadcastGetUsers(){
 		_this.io.sockets.emit(Types.GET_USERS, response);
 	});
 }
+function broadcastGetSessions(){
+	var _this = this,
+		switches = (function(sockets){
+			var res = [];
+			for(socketKey in sockets){
+				var socket = sockets[socketKey];
+				res.push("s" + socket.switch + "a" + socket.attributes.offset + "u" + (socket.attributes.user_id ? socket.attributes.user_id : ""));
+			}
+			return res;
+		})(_this.io.sockets.sockets).filter(function(str, strKey, self){
+			return self.indexOf(str) == strKey;
+		}).map(function(str){
+			return str.match(/s(.*)a(.*)u(.*)/).filter(function(option, key){
+				return [1,2,3].indexOf(key) > -1;
+			});
+		});
+	switches.forEach(function(action, actionKey){
+		_this.reducer(_this.actions[action[0]](!action[2] ? action[1] : {offset: action[1], user_id: action[2]}), function(response){
+			switches[actionKey].push(response);
+			(function(sockets){
+				for(var socketKey in sockets){
+					var socket = sockets[socketKey];
+					socket.emit(socket.switch, switches.filter(function(option){
+						return option[0] == socket.switch && 
+							   option[1] == socket.attributes.offset && 
+							   socket.attributes.user_id ?
+							      option[2] == socket.attributes.user_id :
+							      true;
+					})[0][3]);
+				}
+			})(_this.io.sockets.sockets);
+		});
+	});
+}
 module.exports = function(io, reducer, actions){
 	io.on('connection', function(socket){
 		broadcastGetUsers = broadcastGetUsers.bind({reducer, actions, io});
+		broadcastGetSessions = broadcastGetSessions.bind({reducer, actions, io});
 		socket.on(Types.LOGIN, function(data){
 			socket.email = data.email;
 			reducer(actions.LOGIN(data), function(response){
@@ -38,43 +73,86 @@ module.exports = function(io, reducer, actions){
 			})
 		});
 		socket.on(Types.GET_SESSIONS, function(data){
+			socket.switch = Types.GET_SESSIONS;
+			socket.attributes = {
+				offset: data
+			};
 			reducer(actions.GET_SESSIONS(data), function(response){
 				socket.emit(Types.GET_SESSIONS, response);
-			})
+			});
 		});
 		socket.on(Types.GET_USER_SESSIONS, function(data){
+			socket.switch = Types.GET_USER_SESSIONS;
+			socket.attributes = {
+				offset: data.offset,
+				user_id: data.user_id
+			};
 			reducer(actions.GET_USER_SESSIONS(data), function(response){
 				socket.emit(Types.GET_USER_SESSIONS, response);
 			});
 		});
 		socket.on(Types.GET_FREE_SESSIONS, function(data){
+			socket.switch = Types.GET_FREE_SESSIONS;
+			socket.attributes = {
+				offset: data
+			};
 			reducer(actions.GET_FREE_SESSIONS(data), function(response){
 				socket.emit(Types.GET_FREE_SESSIONS, response);
 			});
 		});
 		socket.on(Types.GET_BUSY_SESSIONS, function(data){
+			socket.switch = Types.GET_BUSY_SESSIONS;
+			socket.attributes = {
+				offset: data
+			};
 			reducer(actions.GET_BUSY_SESSIONS(data), function(response){
 				socket.emit(Types.GET_BUSY_SESSIONS, response);
 			});
 		});
 		socket.on(Types.GET_ERROR_SESSIONS, function(data){
+			socket.switch = Types.GET_ERROR_SESSIONS;
+			socket.attributes = {
+				offset: data
+			};
 			reducer(actions.GET_ERROR_SESSIONS(data), function(response){
 				socket.emit(Types.GET_ERROR_SESSIONS, response);
 			});
 		});
 		socket.on(Types.GET_SUCCESS_SESSIONS, function(data){
+			socket.switch = Types.GET_SUCCESS_SESSIONS;
+			socket.attributes = {
+				offset: data
+			};
 			reducer(actions.GET_SUCCESS_SESSIONS(data), function(response){
 				socket.emit(Types.GET_SUCCESS_SESSIONS, response);
 			});
 		});
 		socket.on(Types.GET_ACTIVE_SESSIONS, function(data){
+			socket.switch = Types.GET_ACTIVE_SESSIONS;
+			socket.attributes = {
+				offset: data
+			};
 			reducer(actions.GET_ACTIVE_SESSIONS(data), function(response){
 				socket.emit(Types.GET_ACTIVE_SESSIONS, response);
 			});
 		});
 		socket.on(Types.GET_INACTIVE_SESSIONS, function(data){
+			socket.switch = Types.GET_INACTIVE_SESSIONS;
+			socket.attributes = {
+				offset: data
+			};
 			reducer(actions.GET_INACTIVE_SESSIONS(data), function(response){
 				socket.emit(Types.GET_INACTIVE_SESSIONS, response);
+			});
+		});
+		socket.on(Types.BIND_SESSION, function(data){
+			reducer(actions.BIND_SESSION(data), function(response){
+				broadcastGetSessions();
+			});
+		});
+		socket.on(Types.UNBIND_SESSION, function(data){
+			reducer(actions.UNBIND_SESSION(data), function(response){
+				broadcastGetSessions();
 			});
 		});
 	});
