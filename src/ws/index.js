d38f9ad185681a14cacf8a98082d847488bdab69
parshencev <parshencev.vlad@gmail.com>
2017-07-27 23:07:44 +0300

    исправление недочетов обновления пользователей и события для получения диалога

--- src/ws/index.js
+++ src/ws/index.js
@@ -9,9 +9,10 @@ function broadcastGetSessions(){
 	var _this = this,
 		switches = (function(sockets){
 			var res = [];
-			for(socketKey in sockets){
+			for(var socketKey in sockets){
 				var socket = sockets[socketKey];
-				res.push("s" + socket.switch + "a" + socket.attributes.offset + "u" + (socket.attributes.user_id ? socket.attributes.user_id : ""));
+				if (socket.switch && socket.switch != Types.GET_SESSION_DIALOG && socket.switch != Types.GET_SESSION_INFO)
+					res.push("s" + socket.switch + "a" + socket.attributes.offset + "u" + (socket.attributes.user_id ? socket.attributes.user_id : ""));
 			}
 			return res;
 		})(_this.io.sockets.sockets).filter(function(str, strKey, self){
@@ -30,23 +31,49 @@ function broadcastGetSessions(){
 					socket.emit(socket.switch, switches.filter(function(option){
 						return option[0] == socket.switch && 
 							   option[1] == socket.attributes.offset && 
-							   socket.attributes.user_id ?
-							      option[2] == socket.attributes.user_id :
-							      true;
+								   socket.attributes.user_id ?
+								      option[2] == socket.attributes.user_id :
+								      true;
 					})[0][3]);
 				}
 			})(_this.io.sockets.sockets);
 		});
 	});
 }
+function broadcastGetSessionInfoAndDialog(){
+	var _this = this,
+		switches = (function(sockets){
+			var res = [];
+			for(var socketKey in sockets){
+				var socket = sockets[socketKey];
+				if(socket.switch && (socket.switch == Types.GET_SESSION_INFO || socket.switch == Types.GET_SESSION_DIALOG))
+					if (res.indexOf(+socket.attributes.session_id) == -1)
+						res.push(socket.attributes.session_id);
+			}
+			return res;
+		})(_this.io.sockets.sockets);
+		switches.forEach(function(action){
+			_this.reducer(_this.actions[Types.GET_SESSION_INFO](action), function(response){
+				for(var socketKey in _this.io.sockets.sockets){
+					var socket = _this.io.sockets.sockets[socketKey];
+					if ((socket.switch == Types.GET_SESSION_DIALOG || socket.switch == Types.GET_SESSION_INFO) && socket.attributes.session_id && socket.attributes.session_id == action){
+						socket.emit(Types.GET_SESSION_INFO, response);
+					}
+				}
+			});
+		});
+}
 module.exports = function(io, reducer, actions){
 	io.on('connection', function(socket){
 		broadcastGetUsers = broadcastGetUsers.bind({reducer, actions, io});
 		broadcastGetSessions = broadcastGetSessions.bind({reducer, actions, io});
+		broadcastGetSessionInfoAndDialog = broadcastGetSessionInfoAndDialog.bind({reducer, actions, io});
 		socket.on(Types.LOGIN, function(data){
 			socket.email = data.email;
+			socket.switch = "";
 			reducer(actions.LOGIN(data), function(response){
 				socket.emit(Types.LOGIN, response);
+				broadcastGetUsers();
 			});
 			reducer(actions.UPDATE_USER(socket.email), function(response){
 				socket.emit(Types.UPDATE_USER, response);
@@ -57,17 +84,20 @@ module.exports = function(io, reducer, actions){
 			broadcastGetUsers();
 		});
 		socket.on(Types.LOGOUT, function(){
+			socket.switch = "";
 			reducer(actions.LOGOUT({email: socket.email}), function(response){
 				socket.emit(Types.LOGOUT, response);
 			});
 			broadcastGetUsers();
 		});
 		socket.on(Types.GET_USERS, function(){
+			socket.switch = "";
 			reducer(actions.GET_USERS(), function(response){
 				socket.emit(Types.GET_USERS, response);
 			})
 		});
 		socket.on(Types.SET_USER, function(data){
+			socket.switch = "";
 			reducer(actions.SET_USER(data), function(){
 				broadcastGetUsers();
 			})
@@ -146,13 +176,34 @@ module.exports = function(io, reducer, actions){
 			});
 		});
 		socket.on(Types.BIND_SESSION, function(data){
+			console.log(socket.switch, socket.attributes.session_id);
 			reducer(actions.BIND_SESSION(data), function(response){
 				broadcastGetSessions();
+				broadcastGetSessionInfoAndDialog();
 			});
 		});
 		socket.on(Types.UNBIND_SESSION, function(data){
 			reducer(actions.UNBIND_SESSION(data), function(response){
 				broadcastGetSessions();
+				broadcastGetSessionInfoAndDialog();
+			});
+		});
+		socket.on(Types.GET_SESSION_INFO, function(data){
+			socket.switch = Types.GET_SESSION_INFO;
+			socket.attributes = {
+				session_id: data
+			};
+			reducer(actions.GET_SESSION_INFO(data), function(response){
+				socket.emit(Types.GET_SESSION_INFO, response);
+			});
+		});
+		socket.on(Types.GET_SESSION_DIALOG, function(data){
+			socket.switch = Types.GET_SESSION_DIALOG;
+			socket.attributes = {
+				session_id: data
+			};
+			reducer(actions.GET_SESSION_DIALOG(data), function(response){
+				socket.emit(Types.GET_SESSION_DIALOG, response);
 			});
 		});
 	});
