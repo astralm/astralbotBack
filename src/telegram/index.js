c149141359ac8961c975bb559d73bbfa5d7baff0
parshencev <parshencev.vlad@gmail.com>
2017-08-01 14:47:14 +0300

    настройка рассылки диалога и расслыка информации о сессиях при записи диалога

--- src/telegram/index.js
+++ src/telegram/index.js
@@ -1,4 +1,4 @@
-module.exports = function(telegram, apiai, reducer, actions){
+module.exports = function(telegram, apiai, reducer, actions, io){
 	telegram.on("message", function(message){
 		if(telegram.connections.filter(function(connection){return connection.hash == message.chat.id;}).length == 0){
 			telegram.connections.push({hash: message.chat.id, bot: true});
@@ -8,11 +8,15 @@ module.exports = function(telegram, apiai, reducer, actions){
 			message: message.text,
 			hash: message.chat.id
 		}));
+		io.broadcastGetSessionsDialog({session_hash: message.chat.id});
+		io.broadcastGetSessions();
 		var request = apiai.textRequest(message.text, {sessionId: message.chat.id});
 		request.on('response', function(response){
 			if(telegram.connections.find(function(connection){return connection.hash == message.chat.id;}).bot){
 				reducer(actions.SET_ANSWER({hash: message.chat.id, message: response.result.fulfillment.speech}));
 				telegram.sendMessage(message.chat.id, response.result.fulfillment.speech);
+				io.broadcastGetSessionsDialog({session_hash: message.chat.id});
+				io.broadcastGetSessions();
 			}
 		});
 		request.end();
