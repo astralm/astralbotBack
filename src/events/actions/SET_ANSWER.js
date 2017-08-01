af33a3cb8861f2ec6d8900427397a21b318ceb78
parshencev <parshencev.vlad@gmail.com>
2017-08-01 12:52:03 +0300

    запись ответа клиента в базу

---
+++ src/events/actions/SET_ANSWER.js
@@ -0,0 +1,16 @@
+module.exports = function(data, callback){
+	this.mysql.query({
+		sql: 'INSERT INTO `answers` (`session_id`, `question_id`, `answer_message`) SELECT s.session_id, (SELECT question_id FROM `questions` WHERE `session_id` = s.session_id ORDER BY `question_id` DESC LIMIT 1), ? FROM `sessions` AS `s` WHERE `session_hash` = ?',
+		timeout: 1000,
+		values: [
+			data.message,
+			data.hash
+		]
+	}, function(err, data){
+		if(callback){
+			err ?
+				callback() :
+				callback(data || null);
+		}
+	});
+}
