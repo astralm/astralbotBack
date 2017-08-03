80f83fd32fc5bb97a774b9da50a134faeb962cb5
parshencev <parshencev.vlad@gmail.com>
2017-08-03 15:19:36 +0300

    событие обновления информации о пользователе

---
+++ src/events/actions/UPDATE_USER_INFORMATION.js
@@ -0,0 +1,18 @@
+module.exports = function(data, callback){
+	var result = Object.keys(data).filter(function(key){
+		return data[key] != null && key != "email_2";
+	});
+	this.mysql.query({
+		sql: 'UPDATE `users` SET ' + result.map(function(item){
+			return "`user_" + item + "` = ?";
+		}).join(", ") + ' WHERE `user_email` = ?',
+		timeout: 1000,
+		values: result.map(function(item){
+			return data[item];
+		}).concat(data.email_2)
+	}, function(err, data){
+		err ?
+			callback() :
+			callback(data || null);
+	});
+}
