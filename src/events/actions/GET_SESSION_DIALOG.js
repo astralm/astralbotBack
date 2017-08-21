4085fdd8b69f95422d8a21092e2d2af6086d8a6b
parshencev <parshencev.vlad@gmail.com>
2017-08-21 17:10:20 +0300

    исправление запроса на получение списка диалга

--- src/events/actions/GET_SESSION_DIALOG.js
+++ src/events/actions/GET_SESSION_DIALOG.js
@@ -3,7 +3,7 @@ module.exports = function(data, callback){
 		sql: 'SELECT *, DATE_FORMAT(`question_date`, \'%Y-%d-%m %H:%i:%s\') AS `question_date_formated`, DATE_FORMAT(`answer_date`, \'%Y-%d-%m %H:%i:%s\') AS `answer_date_formated` FROM `session_dialog_view` WHERE `'+(data.session_id ? 'session_id' : 'session_hash')+'` = ? ORDER BY `question_id`',
 		timeout: 1000,
 		values: [
-			+data.session_id || data.session_hash
+			data.session_id ? +data.session_id : data.session_hash
 		]
 	}, function(err, data){
 		err ?
