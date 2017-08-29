ecb0348a39477edb452ee921854b9dff19d9d8ec
parshencev <parshencev.vlad@gmail.com>
2017-08-29 17:15:20 +0300

    модернизация обработки события на получение списка сессий и установки фильтрации

--- src/ws/index.js
+++ src/ws/index.js
@@ -5,41 +5,6 @@ function broadcastGetUsers(){
 		_this.io.sockets.emit(Types.GET_USERS, response);
 	});
 }
-function broadcastGetSessions(){
-	var _this = this,
-		switches = (function(sockets){
-			var res = [];
-			for(var socketKey in sockets){
-				var socket = sockets[socketKey];
-				if (socket.switch && socket.switch != Types.GET_SESSION_DIALOG && socket.switch != Types.GET_SESSION_INFO)
-					res.push("s" + socket.switch + "a" + socket.attributes.offset + "u" + (socket.attributes.user_id ? socket.attributes.user_id : ""));
-			}
-			return res;
-		})(_this.io.sockets.sockets).filter(function(str, strKey, self){
-			return self.indexOf(str) == strKey;
-		}).map(function(str){
-			return str.match(/s(.*)a(.*)u(.*)/).filter(function(option, key){
-				return [1,2,3].indexOf(key) > -1;
-			});
-		});
-	switches.forEach(function(action, actionKey){
-		_this.reducer(_this.actions[action[0]](!action[2] ? action[1] : {offset: action[1], user_id: action[2]}), function(response){
-			switches[actionKey].push(response);
-			(function(sockets){
-				for(var socketKey in sockets){
-					var socket = sockets[socketKey];
-					socket.emit(socket.switch, switches.filter(function(option){
-						return option[0] == socket.switch && 
-							   option[1] == socket.attributes.offset && 
-								   socket.attributes.user_id ?
-								      option[2] == socket.attributes.user_id :
-								      true;
-					})[0][3]);
-				}
-			})(_this.io.sockets.sockets);
-		});
-	});
-}
 function broadcastGetSessionInfo(session_id){
 	var _this = this;
 	this.reducer(this.actions.GET_SESSION_INFO(session_id), function(response){
@@ -49,6 +14,9 @@ function broadcastGetSessionInfo(session_id){
 				socket.emit(Types.GET_SESSION_INFO, response);
 		}
 	});
+}
+function broadcastGetSessions(data){
+	
 }
 function broadcastGetSessionsDialog(data){
 	var _this = this;
@@ -75,6 +43,9 @@ module.exports = function(io, reducer, actions, telegram, apiai){
 				io.broadcastGetUsers();
 			});
 			reducer(actions.UPDATE_USER(socket.email), function(response){
+				if(response && response[0]){
+					socket.user_id = response[0].user_id;
+				}
 				socket.emit(Types.UPDATE_USER, response);
 			});
 		});
@@ -86,7 +57,6 @@ module.exports = function(io, reducer, actions, telegram, apiai){
 			if(socket.type == "widget"){
 				reducer(actions.SET_INACTIVE({session_hash: socket.attributes.session_hash || socket.token, session_id: socket.attributes.session_id}), function(){
 					io.broadcastGetSessions();
-					console.log(socket.attributes.session_id);
 					io.broadcastGetSessionInfo(socket.attributes.session_id);
 				});
 			}
@@ -113,74 +83,35 @@ module.exports = function(io, reducer, actions, telegram, apiai){
 		socket.on(Types.GET_SESSIONS, function(data){
 			socket.switch = Types.GET_SESSIONS;
 			socket.attributes = {
-				offset: data
+				offset: data.offset,
+				filters: Array.apply(this, data.filters),
+				order: {
+					name: data.order.name,
+					desc: data.order.desc
+				},
+				user_id: socket.user_id
 			};
+			data.user_id = socket.user_id;
 			reducer(actions.GET_SESSIONS(data), function(response){
 				socket.emit(Types.GET_SESSIONS, response);
 			});
 		});
-		socket.on(Types.GET_USER_SESSIONS, function(data){
-			socket.switch = Types.GET_USER_SESSIONS;
-			socket.attributes = {
-				offset: data.offset,
-				user_id: data.user_id
-			};
-			reducer(actions.GET_USER_SESSIONS(data), function(response){
-				socket.emit(Types.GET_USER_SESSIONS, response);
-			});
-		});
-		socket.on(Types.GET_FREE_SESSIONS, function(data){
-			socket.switch = Types.GET_FREE_SESSIONS;
-			socket.attributes = {
-				offset: data
-			};
-			reducer(actions.GET_FREE_SESSIONS(data), function(response){
-				socket.emit(Types.GET_FREE_SESSIONS, response);
-			});
-		});
-		socket.on(Types.GET_BUSY_SESSIONS, function(data){
-			socket.switch = Types.GET_BUSY_SESSIONS;
-			socket.attributes = {
-				offset: data
-			};
-			reducer(actions.GET_BUSY_SESSIONS(data), function(response){
-				socket.emit(Types.GET_BUSY_SESSIONS, response);
-			});
-		});
-		socket.on(Types.GET_ERROR_SESSIONS, function(data){
-			socket.switch = Types.GET_ERROR_SESSIONS;
-			socket.attributes = {
-				offset: data
-			};
-			reducer(actions.GET_ERROR_SESSIONS(data), function(response){
-				socket.emit(Types.GET_ERROR_SESSIONS, response);
-			});
-		});
-		socket.on(Types.GET_SUCCESS_SESSIONS, function(data){
-			socket.switch = Types.GET_SUCCESS_SESSIONS;
-			socket.attributes = {
-				offset: data
-			};
-			reducer(actions.GET_SUCCESS_SESSIONS(data), function(response){
-				socket.emit(Types.GET_SUCCESS_SESSIONS, response);
-			});
-		});
-		socket.on(Types.GET_ACTIVE_SESSIONS, function(data){
-			socket.switch = Types.GET_ACTIVE_SESSIONS;
-			socket.attributes = {
-				offset: data
-			};
-			reducer(actions.GET_ACTIVE_SESSIONS(data), function(response){
-				socket.emit(Types.GET_ACTIVE_SESSIONS, response);
-			});
-		});
-		socket.on(Types.GET_INACTIVE_SESSIONS, function(data){
-			socket.switch = Types.GET_INACTIVE_SESSIONS;
-			socket.attributes = {
-				offset: data
-			};
-			reducer(actions.GET_INACTIVE_SESSIONS(data), function(response){
-				socket.emit(Types.GET_INACTIVE_SESSIONS, response);
+		socket.on("SET_FILTER", function(data){
+			socket.attributes.offset = data.offset;
+			socket.attributes.order = data.order;
+			var key = socket.attributes.filters.indexOf(data.filter);
+			if(data.filter != "all"){
+				if(key >= 0){
+					socket.attributes.filters.splice(key, 1);
+				} else {
+					socket.attributes.filters.push(data.filter);
+				}
+			} else {
+				socket.attributes.filters = [];
+			}
+			console.log(socket.attributes.filters, data.filter, key);
+			reducer(actions.GET_SESSIONS(socket.attributes), function(response){
+				socket.emit(Types.GET_SESSIONS, response);
 			});
 		});
 		socket.on(Types.BIND_SESSION, function(data){
@@ -297,7 +228,6 @@ module.exports = function(io, reducer, actions, telegram, apiai){
 				socket.attributes.session_id = response;
 				reducer(actions.SET_ACTIVE({session_hash: data}), function(){
 					io.broadcastGetSessions();
-					console.log(socket.attributes.session_id);
 					io.broadcastGetSessionInfo(socket.attributes.session_id);
 				});
 			});
