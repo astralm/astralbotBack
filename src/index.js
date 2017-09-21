3c0dd6ba2db6d2d4a9c1991d5856e37e6f6461b0
parshencev <parshencev.vlad@gmail.com>
2017-09-21 18:31:25 +0300

    объявление nodemailer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 var env = require('./constants/env.js'),
-	reducer = require('./reducer.js')(require('mysql').createConnection(env.mysql)),
+	reducer = require('./reducer.js')(require('mysql').createConnection(env.mysql), require('./nodemailer/index.js')(require('nodemailer'), env)),
 	Events = require('./creators/index.js'),
-	rest = require('express')().use(require('body-parser').json()),
 	telegram = new (require('node-telegram-bot-api'))(env.telegram.token, {polling: true}),
 	apiai = require('apiai')(env.apiai.token),
 	io = require('socket.io')(require('http').createServer().listen(env.ws.port));
@@ -20,14 +19,6 @@ require("./telegram/index.js")(
 	Events,
 	io
 );
-require("./rest/index.js")(
-	rest,
-	reducer,
-	Events
-);
-rest.listen(env.rest.port, function(){
-	console.log("rest server start on port " + env.rest.port);
-});
 
 
 
