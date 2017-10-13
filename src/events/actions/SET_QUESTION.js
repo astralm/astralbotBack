ab19b9f69b7ba4218dba0bca78509cde5e37a6e6
parshencev <parshencev.vlad@gmail.com>
2017-10-13 15:38:48 +0300

    включение запроса в запрос

--- src/events/actions/SET_QUESTION.js
+++ src/events/actions/SET_QUESTION.js
@@ -1,4 +1,6 @@
 module.exports = function(data, callback){
+	var _this = this,
+		_data = data;
 	this.mysql.query({
 		sql: 'INSERT INTO `questions` (`session_id`, `question_message`) SELECT s.session_id, ? AS `question_message` FROM `sessions` AS `s` WHERE `session_hash` = ?',
 		timeout: 1000,
@@ -7,17 +9,19 @@ module.exports = function(data, callback){
 			data.hash
 		]
 	}, function(err, data){
+		if(!err){
+			_this.mysql.query({
+				sql: 'UPDATE `sessions` SET `session_dialog_update_date`=NOW() WHERE `session_hash`=?',
+				timeout: 1000,
+				values: [
+					_data.hash
+				]
+			});
+		}
 		if(callback){
 			err ?
 				callback() :
 				callback(data || null);
 		}
-	});
-	this.mysql.query({
-		sql: 'UPDATE `sessions` SET `session_dialog_update_date`=DATE_FORMAT(NOW(), \'%Y-%d-%m %H:%i:%s\') WHERE `session_hash`=?',
-		timeout: 1000,
-		values: [
-			data.hash
-		]
 	});
 }
