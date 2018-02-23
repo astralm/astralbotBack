aae1697eb3ba8413861c194393689467f04006b1
parshencev <parshencev.vlad@gmail.com>
2018-02-23 12:58:04 +0300

    удаление вывода

--- src/then/index.js
+++ src/then/index.js
@@ -3,10 +3,6 @@ let Then = (reducer, responce) => {
 	(responce && responce[2] && responce[2][0] && responce[2][0].a && (responce = responce[2][0].a)) ||
 	(responce && responce[0] && responce[0].a && (responce = responce[0].a)) || (responce = false);
 	if (responce){
-		console.log("\n");
-		console.log("-----------------------------------------------");
-		console.log(responce);
-		console.log("\n");
 		let arr = JSON.parse(responce);
 		for (let i = 0; i < arr.length; i++){
 			let obj = arr[i];
