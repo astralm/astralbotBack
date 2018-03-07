efbcdf51a0e0d12f4ec944c16cb1538cf6b55a3b
parshencev <parshencev.vlad@gmail.com>
2018-03-07 16:14:46 +0300

    небольшой рефакторинг кода и исправление ошибки

--- src/mysql/functions/getAnswerIdForMessage.sql
+++ src/mysql/functions/getAnswerIdForMessage.sql
@@ -19,7 +19,7 @@ BEGIN
                 SET shift = JSON_UNQUOTE(JSON_EXTRACT(messageArray, "$[0][0]"));
                 SET segmentLength = CHAR_LENGTH(messageSegment);
                 SET segmentEssencesArray = getEssencesInText(botID, shift, messageSegment);
-                SET segmentEssencesLength = JSON_LENGTH(segmentEssencesArray);         
+                SET segmentEssencesLength = JSON_LENGTH(segmentEssencesArray);   
                 IF segmentEssencesLength > 0
                     THEN BEGIN
                         SET iterator = 0;
@@ -67,119 +67,121 @@ BEGIN
                             ITERATE cutLoop;
                         END LOOP;
                         SET essencesPositionArray = JSON_MERGE(essencesPositionArray, segmentEssencesArray);
+                        SET iterator = 0;
+                        SET segmentEssencesLength = JSON_LENGTH(segmentEssencesArray);
+                        SET segmentDuplicate = messageSegment;
+                        SET cutWeights = JSON_ARRAY();
+                        cutSegmentLoop: LOOP
+                            IF iterator >= segmentEssencesLength
+                                THEN LEAVE cutSegmentLoop;
+                            END IF;
+                            SET essence = JSON_EXTRACT(segmentEssencesArray, CONCAT("$[", iterator, "]"));
+                            SET essenceStart = JSON_EXTRACT(essence, "$[1][0]");
+                            SET essenceEnd = JSON_EXTRACT(essence, "$[1][1]");
+                            SET essenceLength = essenceEnd - essenceStart + 1;
+                            SET cutWeights = JSON_SET(cutWeights, CONCAT("$[", iterator, "]"), essenceLength);
+                            SET essenceString = SUBSTRING(messageSegment, essenceStart - shift, essenceLength);
+                            SET essenceStart = INSTR(segmentDuplicate, essenceString);
+                            SET segmentDuplicate = INSERT(segmentDuplicate, essenceStart, essenceLength, "{{$$$}}");
+                            SET iterator = iterator + 1;
+                            ITERATE cutSegmentLoop;
+                        END LOOP;
+                        SET iterator = 0;
+                        SET startLocate = 0;
+                        uploadLoop: LOOP
+                            IF iterator >= segmentEssencesLength
+                                THEN LEAVE uploadLoop;
+                            END IF;
+                            SET flagStart = INSTR(segmentDuplicate, "{{$$$}}") - 1;
+                            IF flagStart > 0
+                                THEN BEGIN
+                                    SET cutResultLeft = LEFT(segmentDuplicate, flagStart);
+                                    SET cutLength = CHAR_LENGTH(cutResultLeft);
+                                    SET messageArray = JSON_MERGE(messageArray, JSON_ARRAY(JSON_ARRAY(flagStart + startLocate + shift - cutLength, cutResultLeft)));
+                                    SET startLocate = startLocate + cutLength + JSON_EXTRACT(cutWeights, CONCAT("$[", iterator, "]"));
+                                    SET segmentDuplicate = SUBSTRING(segmentDuplicate, cutLength + 8);
+                                END;
+                                ELSE BEGIN
+                                    SET startLocate = startLocate + JSON_EXTRACT(cutWeights, CONCAT("$[", iterator, "]"));
+                                    SET segmentDuplicate = SUBSTRING(segmentDuplicate, 8);
+                                END;
+                            END IF;
+                            IF iterator = segmentEssencesLength - 1 AND CHAR_LENGTH(segmentDuplicate) > 0
+                                THEN BEGIN 
+                                    SET cutLength = CHAR_LENGTH(segmentDuplicate);
+                                    SET segmentLength = CHAR_LENGTH(messageSegment);
+                                    SET messageArray = JSON_MERGE(messageArray, JSON_ARRAY(JSON_ARRAY(shift + segmentLength - cutLength, segmentDuplicate)));
+                                END;
+                            END IF;
+                            SET iterator = iterator + 1;
+                            ITERATE uploadLoop;
+                        END LOOP;
                     END;
                 END IF;
                 SET messageArray = JSON_REMOVE(messageArray, "$[0]");
-
-                SET iterator = 0;
-                SET segmentEssencesLength = JSON_LENGTH(segmentEssencesArray);
-                SET segmentDuplicate = messageSegment;
-                SET cutWeights = JSON_ARRAY();
-                cutSegmentLoop: LOOP
-                    IF iterator >= segmentEssencesLength
-                        THEN LEAVE cutSegmentLoop;
-                    END IF;
-                    SET essence = JSON_EXTRACT(segmentEssencesArray, CONCAT("$[", iterator, "]"));
-                    SET essenceStart = JSON_EXTRACT(essence, "$[1][0]");
-                    SET essenceEnd = JSON_EXTRACT(essence, "$[1][1]");
-                    SET essenceLength = essenceEnd - essenceStart + 1;
-                    SET cutWeights = JSON_SET(cutWeights, CONCAT("$[", iterator, "]"), essenceLength);
-                    SET essenceString = SUBSTRING(messageSegment, essenceStart - shift, essenceLength);
-                    SET essenceStart = INSTR(segmentDuplicate, essenceString);
-                    SET segmentDuplicate = INSERT(segmentDuplicate, essenceStart, essenceLength, "{{$$$}}");
-                    SET iterator = iterator + 1;
-                    ITERATE cutSegmentLoop;
-                END LOOP;
-                SET iterator = 0;
-                SET startLocate = 0;
-                uploadLoop: LOOP
-                    IF iterator >= segmentEssencesLength
-                        THEN LEAVE uploadLoop;
-                    END IF;
-                    SET flagStart = INSTR(segmentDuplicate, "{{$$$}}") - 1;
-                    IF flagStart > 0
-                        THEN BEGIN
-                            SET cutResultLeft = LEFT(segmentDuplicate, flagStart);
-                            SET cutLength = CHAR_LENGTH(cutResultLeft);
-                            SET messageArray = JSON_MERGE(messageArray, JSON_ARRAY(JSON_ARRAY(flagStart + startLocate + shift - cutLength, cutResultLeft)));
-                            SET startLocate = startLocate + cutLength + JSON_EXTRACT(cutWeights, CONCAT("$[", iterator, "]"));
-                            SET segmentDuplicate = SUBSTRING(segmentDuplicate, cutLength + 8);
-                        END;
-                        ELSE BEGIN
-                            SET startLocate = startLocate + JSON_EXTRACT(cutWeights, CONCAT("$[", iterator, "]"));
-                            SET segmentDuplicate = SUBSTRING(segmentDuplicate, 8);
-                        END;
-                    END IF;
-                    IF iterator = segmentEssencesLength - 1 AND CHAR_LENGTH(segmentDuplicate) > 0
-                        THEN BEGIN 
-                            SET cutLength = CHAR_LENGTH(segmentDuplicate);
-                            SET segmentLength = CHAR_LENGTH(messageSegment);
-                            SET messageArray = JSON_MERGE(messageArray, JSON_ARRAY(JSON_ARRAY(shift + segmentLength - cutLength, segmentDuplicate)));
-                        END;
-                    END IF;
-                    SET iterator = iterator + 1;
-                    ITERATE uploadLoop;
-                END LOOP;
                 ITERATE searchLoop;
             END LOOP;
-        END;
-    END IF;
-    SET essencesLength = JSON_LENGTH(essencesPositionArray);
-    IF essencesLength > 0 AND essencesLength > 1
-        THEN BEGIN
-            essencesBustLoop: LOOP
-                IF essencesLength <= 0
-                    THEN LEAVE essencesBustLoop;
-                END IF;
-                SET iterator = 0;
-                findMinEssenceLoop: LOOP
-                    IF iterator >= essencesLength
-                        THEN LEAVE findMinEssenceLoop;
-                    END IF;
-                    SET essence = JSON_EXTRACT(essencesPositionArray, CONCAT("$[", iterator, "]"));
-                    SET essenceStart = JSON_UNQUOTE(JSON_EXTRACT(essence, "$[1][0]"));
-                    SET essenceInArray = iterator;
-                    IF iterator = 0
-                        THEN BEGIN 
-                            SET minStart = essenceStart;
-                            SET mainEssenceInArray = essenceInArray;
-                        END;
-                        ELSE BEGIN
-                            IF essenceStart < minStart
-                                THEN BEGIN
+            SET essencesLength = JSON_LENGTH(essencesPositionArray);
+            IF essencesLength > 0 AND essencesLength > 1
+                THEN BEGIN
+                    essencesBustLoop: LOOP
+                        IF essencesLength <= 0
+                            THEN LEAVE essencesBustLoop;
+                        END IF;
+                        SET iterator = 0;
+                        findMinEssenceLoop: LOOP
+                            IF iterator >= essencesLength
+                                THEN LEAVE findMinEssenceLoop;
+                            END IF;
+                            SET essence = JSON_EXTRACT(essencesPositionArray, CONCAT("$[", iterator, "]"));
+                            SET essenceStart = JSON_UNQUOTE(JSON_EXTRACT(essence, "$[1][0]"));
+                            SET essenceInArray = iterator;
+                            IF iterator = 0
+                                THEN BEGIN 
                                     SET minStart = essenceStart;
                                     SET mainEssenceInArray = essenceInArray;
                                 END;
+                                ELSE BEGIN
+                                    IF essenceStart < minStart
+                                        THEN BEGIN
+                                            SET minStart = essenceStart;
+                                            SET mainEssenceInArray = essenceInArray;
+                                        END;
+                                    END IF;
+                                END;
                             END IF;
-                        END;
+                            SET iterator = iterator + 1;
+                            ITERATE findMinEssenceLoop;
+                        END LOOP;
+                        SET saveEssences = JSON_MERGE(saveEssences, JSON_EXTRACT(essencesPositionArray, CONCAT("$[", mainEssenceInArray, "][0]")));
+                        SET essencesPositionArray = JSON_REMOVE(essencesPositionArray, CONCAT("$[", mainEssenceInArray, "]"));
+                        SET essencesLength = essencesLength - 1;
+                        ITERATE essencesBustLoop;
+                    END LOOP;
+                END;
+                ELSEIF essencesLength > 0 THEN SET saveEssences = JSON_ARRAY(JSON_EXTRACT(essencesPositionArray, "$[0][0]"));
+            END IF;
+            SET essencesLength = JSON_LENGTH(saveEssences);
+            IF essencesLength > 0
+                THEN BEGIN
+                    SET iterator = 0;
+                    findEntitiesLoop: LOOP
+                        IF iterator >= essencesLength
+                            THEN LEAVE findEntitiesLoop;
+                        END IF;
+                        SET essenceID = JSON_EXTRACT(saveEssences, CONCAT("$[", iterator, "]"));
+                        SELECT entities_id INTO entitiesID FROM entities_essences WHERE essence_id = essenceID AND bot_id = botID;
+                        SET saveEssences = JSON_SET(saveEssences, CONCAT("$[", iterator, "]"), entitiesID);
+                        SET iterator = iterator + 1;
+                        ITERATE findEntitiesLoop;
+                    END LOOP;
+                    SET entitiesString = REPLACE(REPLACE(REPLACE(saveEssences, "[", ""), "]", ""), " ", "");
+                    SELECT answer_id INTO answerID FROM conditions_answers WHERE organization_id = organizationID AND bot_id = botID AND condition_entities = entitiesString;
+                    IF CHAR_LENGTH(entitiesString) > 0
+                        THEN UPDATE messages SET message_value = entitiesString WHERE message_id = messageID;
                     END IF;
-                    SET iterator = iterator + 1;
-                    ITERATE findMinEssenceLoop;
-                END LOOP;
-                SET saveEssences = JSON_MERGE(saveEssences, JSON_EXTRACT(essencesPositionArray, CONCAT("$[", mainEssenceInArray, "][0]")));
-                SET essencesPositionArray = JSON_REMOVE(essencesPositionArray, CONCAT("$[", mainEssenceInArray, "]"));
-                SET essencesLength = essencesLength - 1;
-                ITERATE essencesBustLoop;
-            END LOOP;
-        END;
-        ELSE SET saveEssences = JSON_ARRAY(JSON_EXTRACT(essencesPositionArray, "$[0][0]"));
-    END IF;
-    SET essencesLength = JSON_LENGTH(saveEssences);
-    IF essencesLength > 0
-        THEN BEGIN
-            SET iterator = 0;
-            findEntitiesLoop: LOOP
-                IF iterator >= essencesLength
-                    THEN LEAVE findEntitiesLoop;
-                END IF;
-                SET essenceID = JSON_EXTRACT(saveEssences, CONCAT("$[", iterator, "]"));
-                SELECT entities_id INTO entitiesID FROM entities_essences WHERE essence_id = essenceID AND bot_id = botID;
-                SET saveEssences = JSON_SET(saveEssences, CONCAT("$[", iterator, "]"), entitiesID);
-                SET iterator = iterator + 1;
-                ITERATE findEntitiesLoop;
-            END LOOP;
-            SET entitiesString = REPLACE(REPLACE(REPLACE(saveEssences, "[", ""), "]", ""), " ", "");
-            SELECT answer_id INTO answerID FROM conditions_answers WHERE organization_id = organizationID AND bot_id = botID AND condition_entities = entitiesString;
+                END;
+            END IF;
         END;
     END IF;
     RETURN answerID;
